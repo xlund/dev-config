7844fb5fc789f97b377291dbf8d8b0ada11916b7
Erik Näslund <code@nsld.dev>
2024-03-12 11:03:12 +0100

    Add tressitter

---
+++ nvim/lua/xlund/plugins/nvim-treesitter.lua
@@ -0,0 +1,38 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+
+    treesitter.setup({
+      -- enable syntax highlighting
+      highlight = { enable = true },
+      -- enable indentation
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "html",
+        "css",
+        "markdown",
+        "lua",
+        "gitignore",
+        "go",
+        "rust",
+        "javascript",
+        "typescript",
+      },
+      incremental_selection {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end
+}
