033f905779a45247f6d386fd1f5a09c57dd08ebe
Erik Näslund <code@nsld.dev>
2024-03-12 16:02:54 +0100

    Add green color scheme

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -4,6 +4,12 @@ export WHITE=0xffffffff
 
 
 # --- Red theme ----
-export BAR_COLOR=0xffbb686e
-export ITEM_BG_COLOR=0xffcc898c
-export ACCENT_COLOR=0xffebcccc
+# export BAR_COLOR=0xffbb686e
+# export ITEM_BG_COLOR=0xffcc898c
+# export ACCENT_COLOR=0xffebcccc
+
+
+# --- Green theme ---
+export BAR_COLOR=0xff2f4227
+export ITEM_BG_COLOR=0xff4a5d42
+export ACCENT_COLOR=0xff6f7f68
